1f43e379f75a88c7debe00cb788c4b0db65d2f54
Avery Berryman <averyberryman@gmail.com>
2023-08-07 13:57:04 -0600

    Add first try at post function and .hidden rule for CSS file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,3 +8,21 @@ function getAPIData(dataset) {
     });
 }
 
+function postAPIData(dataset, userData) {
+    const requestData = {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userData)
+      };
+  
+    return fetch(`http://localhost:3001/api/v1/${dataset}`, requestData)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`);
+        }
+        return response.json();
+      });
+  }
+  
+  export { getAPIData, postAPIData };
+  
