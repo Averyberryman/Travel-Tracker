cdb66e2ecbd01da9bf7fe3f72567064dcb3bf406
Avery Berryman <averyberryman@gmail.com>
2023-08-07 19:50:36 -0600

    revise query selectors/namingconventions for elements

--- src/scripts.js
+++ src/scripts.js
@@ -13,15 +13,31 @@ import './images/turing-logo.png'
 
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
-const showHidePassword = document.querySelector('.show-password-button');
+const showHidePasswordButton = document.querySelector('.show-password-button');
 const loginCredentials = Array.from(document.querySelectorAll('.credentials'));
 const loginPage = document.querySelector('.login-page');
 const startPlanningButton = document.querySelector('.user-login-button');
 const loginErrorMsg = document.querySelector('.error-msg');
-const tryAgainButton = document.querySelector('.try-again-btn');
+const tryAgainButton = document.querySelector('.try-again-button');
 const mainPage = document.querySelector('main');
+const userFirstName = document.querySelector('.traveler-name');
+const dateAsOfToday = document.querySelector('.date-today');
+const userTotalSpent = document.querySelector('.total-spent');
+const userPastTrips = document.querySelector('.past-trips-container');
+const userPendingTrips = document.querySelector('.pending-trips-container');
+const userUpcomingTrips = document.querySelector('.upcoming-trips-container');
+const requestForm = document.querySelector('.trip-request-form');
+const tripDate = document.querySelector('#trip-date');
+const tripLength = document.querySelector('#trip-length');
+const partySize = document.querySelector('#party-size');
+const tripRequestDestinations = document.querySelector('#request-destinations');
+const tripRequestInputs = Array.from(document.querySelectorAll('.trip-request-input'));
+const tripEstimateShow = document.querySelector('.trip-estimate-show');
+const tripEstimate = document.querySelector('.trip-estimate');
+const requestTripButton = document.querySelector('.request-trip-button');
+const postResponseShow = document.querySelector('.post-response-show');
+const postResponseMsg = document.querySelector('.post-response-msg');
+const requestFormResetButton = document.querySelector('.reset-request-form-button');
+const pageReload = document.querySelector('.page-reload');
 
 
-
-
-console.log('This is the JavaScript entry file - your code begins here.');
